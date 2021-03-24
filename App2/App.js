3e45bcecaae85f5bbc94b597af73eb1cda19121b
Bergamo11 <bernatowicz122@gmail.com>
2021-03-24 19:43:56 +0100

    MESSAGE

--- App2/App.js
+++ App2/App.js
@@ -6,106 +6,58 @@
  * @flow strict-local
  */
 
-import React from 'react';
+import React, {useState} from 'react';
 import type {Node} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import {StyleSheet,Text,View, TextInput, Button} from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
 
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
+const App: () => Node = () => {
+
+
+
+
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
+    /* tu to co sie wyswietla / renderuje*/
+    <View style ={{padding: 20,flexDirection: 'row'}}>
+    <View>
+    <Text style={{backgroundColor: 'grey',padding:30, width:110, height:100,textAlign: 'center',}}>1</Text>
+    </View>
+    <View>
+    <Text style={{backgroundColor: 'blue',padding:30, width:110,height:100, textAlign: 'center',}}>2</Text>
+    </View>
+    <View>
+    <Text style={{backgroundColor: 'green',padding:30, width:110, height:100,textAlign: 'center',}}>3</Text>
+    </View>
     </View>
   );
 };
 
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-};
-
 const styles = StyleSheet.create({
-  sectionContainer: {
+  mainContainer: {
     marginTop: 32,
-    paddingHorizontal: 24,
+    backgroundColor: 'grey',
+    flex: 1,
+    padding: 50,
   },
   sectionTitle: {
     fontSize: 24,
     fontWeight: '600',
+  textAlign: 'center',
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
+  sectionText: {
+    fontSize: 36,
+    fontWeight: '600',
+  textAlign: 'center',
   },
-  highlight: {
-    fontWeight: '700',
+  buttonContainer: {
+    justifyContent: 'center',
+    alignContent: 'center',
+    width: 150,
+    height: 50,
+    borderRadius: 10,
+    borderWidth: 3,
+    borderColor: 'black',
+    backgroundColor: 'blue',
   },
 });
 
